e28ac6515d7a9d63f3598eb14c2ebd0bf7f1955a
hello <hello@ifnot.cc>
2016-11-25 18:14:19 +0800

    add firebase database

--- app/src/main/java/cc/ifnot/todoparse/activity/MainActivity.java
+++ app/src/main/java/cc/ifnot/todoparse/activity/MainActivity.java
@@ -6,6 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.orhanobut.logger.Logger;
 import com.parse.ParseUser;
 
 import cc.ifnot.todoparse.R;
@@ -20,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseAnalytics firebaseAnalytics = TodoApp.getTodoApp().getFirebaseAnalytics();
         firebaseAnalytics.setAnalyticsCollectionEnabled(true);
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference message = firebaseDatabase.getReference("message");
+        message.setValue("Hello world!");
+        message.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Logger.d("value changed: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Logger.w("Failed to read value: ", databaseError.toException());
+            }
+        });
 
         if(ParseUser.getCurrentUser() == null){
             Intent intent = new Intent(this, LoginActivity.class);
